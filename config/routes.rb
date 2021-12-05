bd7c052413bcf9f2e5a3ebf5a66021737246123d
MarkBezsonov <markbezsonov@gmail.com>
2021-12-05 01:03:32 -0600

    Update main content

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'about_me', to: 'pages#about_me'
-  get 'my_credentials', to: 'pages#my_credentials'
-  get 'my_work', to: 'pages#my_work'
-  get 'contact_me', to: 'pages#contact_me'
 end
